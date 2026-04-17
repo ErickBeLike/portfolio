479e9a6e627f4cde54d984eccf98cdc4ecc026e3
ErickBeLike <20223l001053@utcv.edu.mx>
2026-04-16 21:03:17 -0600

    feat: enhance hero component with dynamic theme label and update tech stack with new technologies

--- src/app/components/tech-stack/tech-stack.component.ts
+++ src/app/components/tech-stack/tech-stack.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { TranslocoModule } from '@ngneat/transloco'; // <-- 1. IMPORTA
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-tech-stack',
   standalone: true,
   imports: [TranslocoModule], // <-- 2. AÑADE
+  schemas: [NO_ERRORS_SCHEMA],
   templateUrl: './tech-stack.component.html',
   styleUrl: './tech-stack.component.scss'
 })
