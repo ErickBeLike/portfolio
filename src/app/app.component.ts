11a08227825ce2690307ed8d5909b90b0ffe96b3
ErickBeLike <20223l001053@utcv.edu.mx>
2026-04-16 20:01:47 -0600

    feat: add new experience entries and implement language detection for improved localization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslocoService } from '@ngneat/transloco';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
+
+const MEXICO_TIMEZONES = new Set([
+  'America/Mexico_City',
+  'America/Cancun',
+  'America/Merida',
+  'America/Monterrey',
+  'America/Matamoros',
+  'America/Mazatlan',
+  'America/Chihuahua',
+  'America/Ojinaga',
+  'America/Hermosillo',
+  'America/Tijuana',
+  'America/Ensenada',
+  'America/Santa_Isabel',
+  'America/Bahia_Banderas',
+]);
 
 @Component({
   selector: 'app-root',
@@ -9,6 +26,14 @@ import { FooterComponent } from "./components/footer/footer.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+
+  constructor(private transloco: TranslocoService) {}
+
+  ngOnInit(): void {
+    const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const lang = MEXICO_TIMEZONES.has(tz) ? 'es' : 'en';
+    this.transloco.setActiveLang(lang);
+  }
 }
